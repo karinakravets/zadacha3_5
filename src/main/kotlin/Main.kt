5fc101983438a9a3fefbd83c3057017527a6a55c
karina <135805670+karinakravets@users.noreply.github.com>
2023-11-15 00:21:18 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,32 @@
-//Создать приложение, в котором пользователь вводит массив из различных слов.
-//На выходе приложение должно показать слова сгруппированные по признаку
-//"состоят из одинаковых букв". Например, на входе ["eat", "tea", "tan", "ate", "nat", "bat"].
-// Получаем группы:
+//"eat", "tea", "tan", "ate", "nat", "bat"
 //"ate", "eat", "tea"
 //"nat", "tan"
 //"bat"
 
 fun main() {
-println("Введите слова, разделенные пробелами:")
-val input = readLine()
-val words = input?.split(" ")// разбитие строки на отдельные слова с пробелами
-if (words != null) {
-val wordGroups = groupWords(words)
-
-for ((_, group) in wordGroups) { //_ -это пустое значение, т.к. ключ нам не нужен, нужно только значение
-println(group.joinToString(", "))
-}
-}
-}
-fun groupWords(words: List): Map> {
-val wordGroups = mutableMapOf>()
-for (word in words) {
-val key = word.toCharArray().sorted().joinToString("")
-if (!wordGroups.containsKey(key)) {
-wordGroups[key] = mutableListOf()
-}
-wordGroups[key]?.add(word)
+    val input = arrayOf("eat", "tea", "tan", "ate", "nat", "bat")
+    val groups = groupAnagrams(input)
+    for (group in groups) {
+        println(group.joinToString(", "))
+    }
 }
 
-return wordGroups
+fun groupAnagrams(words: Array<String>): List<List<String>> {
+    val groups = mutableListOf<List<String>>()
+    val map = mutableMapOf<String, MutableList<String>>()
+
+    for (word in words) {
+        val sortedWord = word.toCharArray().sorted().joinToString("")
+        if (map.containsKey(sortedWord)) {
+            map[sortedWord]?.add(word)
+        } else {
+            map[sortedWord] = mutableListOf(word)
+        }
+    }
+
+    for (entry in map) {
+        groups.add(entry.value)
+    }
+
+    return groups
 }
